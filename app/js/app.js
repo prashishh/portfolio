3c899f584280aa6e4a2cee5d4469a49b4fbdb37e
Prashish Rajbhandari <prashishh@gmail.com>
2014-10-07 01:07:51 -0400

    rough - all

--- app/js/app.js
+++ app/js/app.js
@@ -2,17 +2,58 @@
 
 $(function() {
 	$(".payclip_more").hide();
+	$(".remo_more").hide();
+	$(".mic_more").hide();
+	$(".fortystones_more").hide();
+	$(".scoop_more").hide();
+	$(".shrii_more").hide();
+
 
 	$(".payclip_button").on('click', function() {
-		var displayProp = $(".payclip_more").css("display");
-		if(displayProp == "none")
-			moreLess = 'Less'
+		var payclip_displayProp = $(".payclip_more").css("display");
+		if(payclip_displayProp == "none")
+			payclip_moreLess = 'Less'
 		else 
-			moreLess = 'More';
+			payclip_moreLess = 'More';
 		
-		console.log(displayProp);
-		$(this).text(moreLess);
+		$(this).text(payclip_moreLess);
 
 		$(".payclip_more").slideToggle();
-		});
+	});
+
+	$(".remo_button").on('click', function() {
+		var remo_displayProp = $(".remo_more").css("display");
+		if(remo_displayProp == "none")
+			remo_moreLess = 'Less'
+		else 
+			remo_moreLess = 'More';
+		
+		$(this).text(remo_moreLess);
+
+		$(".remo_more").slideToggle();
+	});
+
+	$(".mic_button").on('click', function() {
+		var mic_displayProp = $(".mic_more").css("display");
+		if(mic_displayProp == "none")
+			mic_moreLess = 'Less'
+		else 
+			mic_moreLess = 'More';
+		
+		$(this).text(mic_moreLess);
+
+		$(".mic_more").slideToggle();
+	});
+
+	$(".fortystones_button").on('click', function() {
+		var fortystones_displayProp = $(".fortystones_more").css("display");
+		if(fortystones_displayProp == "none")
+			fortystones_moreLess = 'Less'
+		else 
+			fortystones_moreLess = 'More';
+		
+		$(this).text(fortystones_moreLess);
+
+		$(".fortystones_more").slideToggle();
+	});
 });
